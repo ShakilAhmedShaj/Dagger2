b4b0f26c119b318e82926165f17882d6e9e130c4
SHAZ <shakilahmedshaj@gmail.com>
2021-04-20 13:32:05 +0600

    singleton works

--- app/src/main/java/com/decimalab/dagger2/MyApp.kt
+++ app/src/main/java/com/decimalab/dagger2/MyApp.kt
@@ -3,6 +3,9 @@ package com.decimalab.dagger2
 import android.app.Application
 import com.decimalab.dagger2.data.database.DatabaseService
 import com.decimalab.dagger2.data.netwotk.NetworkService
+import com.decimalab.dagger2.di.component.DaggerApplicationComponent
+import com.decimalab.dagger2.di.module.ApplicationModule
+import javax.inject.Inject
 
 /**
  * Created by Shakil Ahmed Shaj on 18,April,2021.
@@ -10,12 +13,19 @@ import com.decimalab.dagger2.data.netwotk.NetworkService
  */
 class MyApp : Application() {
 
+    @Inject
     lateinit var databaseService: DatabaseService
+
+    @Inject
     lateinit var networkService: NetworkService
 
     override fun onCreate() {
         super.onCreate()
-        databaseService = DatabaseService(this, "local_db", 1)
-        networkService = NetworkService(this, "www.shaj.com", "123456")
+
+        DaggerApplicationComponent.builder()
+            .applicationModule(ApplicationModule(this))
+            .build()
+            .inject(this)
+
     }
 }
