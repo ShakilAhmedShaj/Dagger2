32252442ad318672088825a7af86e7ae495a7bc7
SHAZ <shakilahmedshaj@gmail.com>
2021-04-20 13:38:42 +0600

    Revert "singleton works"
    This reverts commit b4b0f26c

--- app/src/main/java/com/decimalab/dagger2/MyApp.kt
+++ app/src/main/java/com/decimalab/dagger2/MyApp.kt
@@ -3,9 +3,6 @@ package com.decimalab.dagger2
 import android.app.Application
 import com.decimalab.dagger2.data.database.DatabaseService
 import com.decimalab.dagger2.data.netwotk.NetworkService
-import com.decimalab.dagger2.di.component.DaggerApplicationComponent
-import com.decimalab.dagger2.di.module.ApplicationModule
-import javax.inject.Inject
 
 /**
  * Created by Shakil Ahmed Shaj on 18,April,2021.
@@ -13,19 +10,12 @@ import javax.inject.Inject
  */
 class MyApp : Application() {
 
-    @Inject
     lateinit var databaseService: DatabaseService
-
-    @Inject
     lateinit var networkService: NetworkService
 
     override fun onCreate() {
         super.onCreate()
-
-        DaggerApplicationComponent.builder()
-            .applicationModule(ApplicationModule(this))
-            .build()
-            .inject(this)
-
+        databaseService = DatabaseService(this, "local_db", 1)
+        networkService = NetworkService(this, "www.shaj.com", "123456")
     }
 }
